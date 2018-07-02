38ab67436f68e0297f245255a906585d9e2e1630
Conor Anderson <conor@conr.ca>
2018-07-02 18:40:45 -0400

    Appveyor fixes and travis.yml [skip ci]

--- .ci/move-conda-package.py
+++ .ci/move-conda-package.py
@@ -1,18 +1,13 @@
-import sys
 import os
 import re
-import yaml
 import glob
 import shutil
 from conda_build.config import Config
 
 conddir = getattr(Config(), "bldpkgs_dir")
+dirs = [i for i in os.listdir() if not re.search(r'\.',i)]
 
-for dir in [i for i in os.listdir() if not re.search(r'\.',i)]:
-  with open(os.path.join(dir, 'meta.yaml')) as f:
-      name = yaml.load(f)['package']['name']
-
-  binary_package_glob = os.path.join(conddir, '{0}*.tar.bz2'.format(name))
+for dir in dirs:
+  binary_package_glob = os.path.join(conddir, '{0}*.tar.bz2'.format(dir))
   binary_package = glob.glob(binary_package_glob)[0]
-
   shutil.move(binary_package, '.')
