7ef0abba493a227723bbedce5e2c0753125b8863
Conor Anderson <conor@conr.ca>
2018-07-02 18:11:14 -0400

    Appveyor changes 5 [skip ci]

--- .ci/move-conda-package.py
+++ .ci/move-conda-package.py
@@ -4,13 +4,15 @@ import re
 import yaml
 import glob
 import shutil
-from conda_build.config import config
+from conda_build.config import Config
+
+conddir = getattr(Config(), "bldpkgs_dir")
 
 for dir in [i for i in os.listdir() if not re.search(r'\.',i)]:
   with open(os.path.join(dir, 'meta.yaml')) as f:
       name = yaml.load(f)['package']['name']
 
-  binary_package_glob = os.path.join(config.bldpkgs_dir, '{0}*.tar.bz2'.format(name))
+  binary_package_glob = os.path.join(conddir, '{0}*.tar.bz2'.format(name))
   binary_package = glob.glob(binary_package_glob)[0]
 
   shutil.move(binary_package, '.')
---
+++ README.md
@@ -0,0 +1,9 @@
+# conda-recipies
+
+Linux builds (CircleCI):
+[![CircleCI](https://circleci.com/gh/ConorIA/conda-recipies.svg?style=svg)](https://circleci.com/gh/ConorIA/conda-recipies)
+
+Windows builds (Appveyor):
+[![Build status](https://ci.appveyor.com/api/projects/status/1oxwxq3tcm61dkd4?svg=true)](https://ci.appveyor.com/project/ConorIA/conda-recipies)
+
+MacOS builds (Travis):
